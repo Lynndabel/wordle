0fc8c950d2719d0b2cd785565f48d83b2eb8ce63
lynn <okolo.ebelelynda@gmail.com>
2026-01-05 11:40:22 +0100

    Home Tab  updated

--- eldrow/src/components/ui/tabs/HomeTab.tsx
+++ eldrow/src/components/ui/tabs/HomeTab.tsx
@@ -12,12 +12,13 @@
  * <HomeTab />
  * ```
  */
+import WordleGame from "../../WordleGame";
+
 export function HomeTab() {
   return (
     <div className="flex items-center justify-center h-[calc(100vh-200px)] px-6">
       <div className="text-center w-full max-w-md mx-auto">
-        <p className="text-lg mb-2">Put your content here!</p>
-        <p className="text-sm text-gray-500 dark:text-gray-400">Powered by Neynar 🪐</p>
+        <WordleGame />
       </div>
     </div>
   );
