9e1297c5dea30a250eb8c4a1ba536d3e1db07662
lynn <okolo.ebelelynda@gmail.com>
2026-01-15 03:27:47 +0100

    color fixed

--- eldrow/src/components/Leaderboard.tsx
+++ eldrow/src/components/Leaderboard.tsx
@@ -24,7 +24,7 @@ export default function Leaderboard() {
   }, []);
 
   return (
-    <div className="my-4 p-4 bg-gray-20 rounded shadow">
+    <div className="my-4 p-4 bg-gray-70 rounded shadow">
       <h3 className="font-bold mb-2">Leaderboard</h3>
       {error && <div className="text-xs text-red-600 mb-2">{error}</div>}
       <table className="w-full text-left">
