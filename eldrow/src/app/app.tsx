0796078027dd3d18a443be962339f5d59e27a64c
Ebele Lynda Okolo <okolo.ebelelynda@gmail.com>
2026-01-23 23:07:59 +0100

    Fix comment typo in app.tsx

--- eldrow/src/app/app.tsx
+++ eldrow/src/app/app.tsx
@@ -3,7 +3,7 @@
 import dynamic from "next/dynamic";
 import { APP_NAME } from "~/lib/constants";
 
-// note: dynamic import is required for components that use the Frame SDK
+// note: dynamic import is required for components that use the Frame SDK..
 const AppComponent = dynamic(() => import("~/components/App"), {
   ssr: false,
 });
